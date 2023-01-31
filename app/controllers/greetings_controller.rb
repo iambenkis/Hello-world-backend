af4c14651288d9cb016973bc8f11cacae022708c
benjamin kisenge <benkisenge03@gmail.com>
2023-01-31 17:36:24 +0200

    Add create greating controller

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,6 @@
+class GreetingsController < ApplicationController
+    def show
+        @greeting = Greeting.find(rand(1..5))
+        render json: @greeting, status: :ok, location: @greeting
+    end
+end
